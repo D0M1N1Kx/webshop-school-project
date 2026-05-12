673307253f4418434d6f451f735d2ab97ec2e3cc
D0M1N1Kx <milkovicsd32@gmail.com>
2026-05-12 20:42:14 +0200

    add navbar for index.php

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 require_once "db_config.php";
 require_once "services/UserService.php";
-
-$userService = new UserService($connection);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,11 +8,29 @@ $userService = new UserService($connection);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Webshop</title>
 </head>
 
 <body>
+    <nav>
+        <?php
+        echo '<a href="index.php">Főoldal</a>';
 
+        if (isset($_SESSION['user_id'])) {
+            echo '<span>Szia, ' . htmlspecialchars($_SESSION['username']) . '!</span>';
+            echo '<a href="pages/cart.php">Kosár</a>';
+
+            if ($_SESSION['role'] === 'admin') {
+                echo '<a href="pages/admin.php">Admin</a>';
+            }
+
+            echo '<a href="pages/logout.php">Kilépés</a>';
+        } else {
+            echo '<a href="pages/login.php">Bejelentkezés</a>';
+            echo '<a href="pages/register.php">Regisztráció</a>';
+        }
+        ?>
+    </nav>
 </body>
 
 </html>
